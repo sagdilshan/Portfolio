28e203900fa409635219da71f0619a7973f62f9a
Gayathra Dilshan <gayathradilshan1@gmail.com>
2024-04-22 18:47:08 +0530

    Update footer.blade.php

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2024 | <a target="_blank" href="https://github.com/sagdilshan/Woodify-Website.git" style="color: red">Woodify - Crafting Dreams into Wooden Reality</a> |
+    <strong>Copyright &copy; 2023 | <a target="_blank" href="https://github.com/sagdilshan/Woodify-Website.git" style="color: red">Woodify - Crafting Dreams into Wooden Reality</a> |
         All Rights Reserved. </strong>
     <div class="float-right d-none d-sm-inline-block">
       Developed By <b>Gayathra Dilshan</b>
